0d2771cbe5197fcbce6de365539286003f05b9a2
kairi9 <53260218+kairi9@users.noreply.github.com>
2021-04-13 12:05:15 +0900

    second_commit

--- MyWebSite/settings.py
+++ MyWebSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&7_varmk^ez&@bwne$wkzc5$*ifrd9yfaba@kfks7dqzn@&30*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-54-238-57-49.ap-northeast-1.compute.amazonaws.com','127.0.0.1']
 
 
 # Application definition
