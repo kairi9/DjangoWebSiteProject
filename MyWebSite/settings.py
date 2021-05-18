593d89f635a182c8d6d88b373827476e96836b09
kairi9 <53260218+kairi9@users.noreply.github.com>
2021-05-18 23:19:52 +0900

    settingCommit

--- MyWebSite/settings.py
+++ MyWebSite/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'myNote:myPage'
 LOGIN_URL = 'accounts:login'
 AUTH_USER_MODEL = 'accounts.MyUser'
 LOGOUT_REDIRECT_URL='accounts:login'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
